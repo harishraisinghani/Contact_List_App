f8b78cbe79448c65bfa88d07f381fb8f10173d89
harishraisinghani <harishraisinghani@gmail.com>
2016-08-03 02:39:47 +0000

    refactored contact code to use a database built with custom ORM

--- contact.rb
+++ contact.rb
@@ -1,17 +1,27 @@
 require 'csv'
+require 'pg'
+
 
 # Represents a person in an address book.
 # The ContactList class will work with Contact objects instead of interacting with the CSV file directly
 class Contact
+
+  @@connection = nil
+  def self.connection
+    @@connection = @@connection || PG.connect(host: 'localhost', dbname: 'contacts', user: 'development', password: 'development')
+  end
+
+  attr_reader :id
 
   attr_accessor :name, :email
   
   # Creates a new contact object
   # @param name [String] The contact's name
   # @param email [String] The contact's email address
-  def initialize(name, email)
+  def initialize(name, email, id=nil)
     @name = name
     @email = email
+    @id = id
   end
 
   # Provides functionality for managing contacts in the csv file.
@@ -20,7 +30,8 @@ class Contact
     # Opens 'contacts.csv' and creates a Contact object for each line in the file (aka each contact).
     # @return [Array<Contact>] Array of Contact objects
     def all
-      CSV.read('contacts.csv')
+      #CSV.read('contacts.csv') - old method
+      Contact.connection.exec('SELECT * FROM contacts ORDER BY id;')
     end
 
     # Creates a new contact, adding it to the csv file, returning the new contact.
@@ -28,34 +39,44 @@ class Contact
     # @param email [String] the contact's email
     def create(name, email)
       contact = Contact.new(name,email)
-      CSV.open('contacts.csv', 'a') do |row|
-        row << [name,email]
-      end
-      contact
+      contact.save
+      
+      #contact = Contact.new(name,email)
+      #CSV.open('contacts.csv', 'a') do |row|
+      #  row << [name,email]
+      #end
+      #contact
     end
     
     # Find the Contact in the 'contacts.csv' file with the matching id.
     # @param id [Integer] the contact id
     # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
     def find(id)
-      arr = Contact.all
-      arr.at(id.to_i-1)
+      begin
+        res = Contact.connection.exec_params('SELECT * FROM contacts WHERE id = $1::int;', [id])[0]
+      rescue
+        return nil
+      end
+      Contact.new(res["name"], res["email"],res["id"])
     end
     
     # Search for contacts by either name or email.
     # @param term [String] the name fragment or email fragment to search for
     # @return [Array<Contact>] Array of Contact objects.
     def search(term)
-      arr = Contact.all
-      filter_arr = []
-      arr.each do |row|
-        if (row[0].include?(term.to_s) || row[1].include?(term.to_s))
-          filter_arr << "#{arr.index(row)+1}: #{row[0]} (#{row[1]})"
-        end          
-      end
-      filter_arr
+      result = Contact.connection.exec_params("SELECT * FROM contacts WHERE (name LIKE $1) OR (email LIKE $1);", ["%#{term}%"])
     end
-
   end
 
+  def save
+    if id.nil?
+      self.class.connection.exec_params('INSERT INTO contacts (name, email) VALUES ($1, $2);' , [name, email])
+    else
+      self.class.connection.exec_params('UPDATE contacts SET name=$1, email=$2 WHERE id=$3::int;', [name, email, id])
+    end
+  end
+
+  def destroy
+    Contact.connection.exec_params('DELETE FROM contacts WHERE id = $1::int;', [id])
+  end
 end
--- contact_list.rb
+++ contact_list.rb
@@ -1,4 +1,5 @@
 require_relative 'contact'
+require 'pry-byebug'
 
 # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
@@ -11,37 +12,68 @@ class ContactList
       puts "Enter new contact's full email address:"
       email = STDIN.gets.chomp
       Contact.create(name,email)
-      puts "Your contact was succesfully created with ID #{Contact.all.length}"
+      puts "Your contact was succesfully created with ID #{Contact.all.count}"
     end
 
     def list
-      arr = Contact.all 
+      arr = Contact.all
       arr.each do |row|
-        puts "#{arr.index(row)+1}: #{row[0]} (#{row[1]})"
+        puts "#{row["id"]}: #{row["name"]} (#{row["email"]})"
       end
-      puts "---\n #{arr.length} record#{'s' unless arr.length == 1} total"
+      puts "---\n #{arr.count} record#{'s' unless arr.count == 1} total"
+    end
+
+    def valid_id?(id)
+      if id == nil
+        puts "Please enter an ID number with this command"
+        return false
+      else
+        @the_contact = Contact.find(id)
+        
+        if @the_contact == nil
+          puts "The contact with that ID not found"
+          return false
+        end
+      end
+      true
     end
 
     def show
       id = ARGV[1]
-      if id == nil
-        puts "Please enter an ID number with the show command"
-      else
-        contact = Contact.find(id)
-        if contact == nil
-          puts "The contact with that ID not found"
-        else
-          puts contact
-        end
+      if valid_id?(id)
+        puts "#{@the_contact.id}: #{@the_contact.name} (#{@the_contact.email})" 
       end
     end
 
     def search
       term = ARGV[1]
       result = Contact.search(term)
-      puts result
-      puts "---\n #{result.length} record#{'s' unless result.length == 1} total"
+      result.each { |row| puts "#{row["id"]}: #{row["name"]} (#{row["email"]})"}
+      puts "---\n #{result.count} record#{'s' unless result.count == 1} total"
     end
+
+    def update
+      id = ARGV[1]
+      if valid_id?(id)
+        puts "Enter updated contact's name:"
+        new_name = STDIN.gets.chomp
+        puts "Enter updated contact's email address:"
+        new_email = STDIN.gets.chomp
+        @the_contact.name = new_name
+        @the_contact.email = new_email
+        @the_contact.save
+        puts "Contact #{@the_contact.id}: #{@the_contact.name} (#{@the_contact.email}) updated"
+      end
+    end
+
+    def destroy
+      id = ARGV[1]
+      if valid_id?(id)
+        @the_contact.destroy
+        puts "Contact successfully removed"
+      end
+    end
+
   end
 end
 
@@ -54,5 +86,7 @@ when 'list' then ContactList.list
 when 'new' then ContactList.new_entry
 when 'show' then ContactList.show
 when 'search' then ContactList.search
-else puts "Here is a list of available commands:\n\tnew\t-Create a new contact\n\tlist\t-List all contacts\n\tshow\t-Show a contact\n\tsearch\t-Search contacts\n"
+when 'update' then ContactList.update
+when 'destroy' then ContactList.destroy
+else puts "Here is a list of available commands:\n\tnew\t-Create a new contact\n\tlist\t-List all contacts\n\tshow\t-Show a contact\n\tsearch\t-Search contacts\n\tupdate\t-Update a contact\n\tdestroy\t-Remove a contact\n"
 end
